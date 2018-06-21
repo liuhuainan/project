725e7ee6a04458aa2b3219bc7090820886355b2f
liuhuainan <liuhuainan169@.com>
2018-06-21 23:41:41 +0800

    后台会员管理

--- web/myadmin/urls.py
+++ web/myadmin/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url
-from . import views
+from . views import views,userviews
 
 urlpatterns = [
-    # url(r'^', views.index,name='myhome_index'),  
-    url(r'^', views.index,name='admin_index'),
+    # 后台首页
+    url(r'^$', views.index,name='myadmin_index'),
+    # 会员管理
+    url(r'^user/add/$', userviews.add,name='myadmin_user_add'),
+    url(r'^user/index/$', userviews.index,name='myadmin_user_list'),
+    url(r'^user/delete/$', userviews.delete,name='myadmin_user_delete'),
+    url(r'^user/edit/$', userviews.edit,name='myadmin_user_edit'),                
 ]
--- web/web/urls.py
+++ web/web/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import url,include
 # from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include('myadmin.urls')),
+    url(r'^myadmin/', include('myadmin.urls')),
     url(r'^', include('myhome.urls')),     
 ]
